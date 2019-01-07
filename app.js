0cfebb11d5cd4567bba7767852d29f922732b4e5
DESKTOP-543E02K\ucreateuser <mohit@ucreate.co.in>
2019-01-07 14:48:57 +0530

    Modify app settings , update some changes on hello app

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var expressValidator=require('express-validator');
-var expressSession=require('express-session');
-var dotenv = require('dotenv');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressValidator=require('express-validator');
+const expressSession=require('express-session');
+const dotenv = require('dotenv');
 dotenv.load();
 
-var migration = require('./Models/classes/migration_db');
-
-var index = require('./routes/index');
-var register = require('./routes/register');
-//var mailgun = require('./routes/mailgun');
-
-var app = express();
-
-// view engine setup
+const index = require('./routes/index');
+const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.engine('html', require('hbs').__express);
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,12 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressSession({secret:'max', saveUninitialized:false,resave:false }));
 app.use('/', index);
-app.use('/',register);
-//app.use('/',mailgun);
 app.use('/admin',require('./routes/admin/login'));
-app.use('/admin',require('./routes/admin/dashboard'));
 app.use('/api',require('./routes/api/users'));
-app.use('/api',require('./routes/api/chat'));
 
 app.get('/favicon.ico', function(req, res) {
     res.send(200);
@@ -47,12 +33,12 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use('/out', express.static(__dirname + '/out'));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-//var RollbarSecretKey = process.env.ROLLBAR_ACCESS_TOKEN;
+//const RollbarSecretKey = process.env.ROLLBAR_ACCESS_TOKEN;
 //app.use(rollbar.errorHandler(RollbarSecretKey));
 
 //app.listen(6943);
