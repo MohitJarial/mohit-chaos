eeb0aa6f23984aa22e27ff4f0b9d93ba849cbb6d
DESKTOP-543E02K\ucreateuser <mohit@ucreate.co.in>
2019-01-11 11:54:16 +0530

    Removed un used method and variable from uat

--- Models/users.js
+++ Models/users.js
@@ -4,8 +4,7 @@
 * @constructor
 */
 
-var config = require('../Models/config');
-var objcommon = require('../Models/common');
+var config = require('../models/config');
 var Cryptr = require('cryptr'), cryptr = new Cryptr(process.env.DB_PASSWORD);
 const pg = require('pg');
 
@@ -14,19 +13,3 @@ const pg = require('pg');
 var dbConnectionConfig = { host:config.db.host, user:config.db.user, password:config.db.password, database:config.db.database, port:config.db.port, ssl:config.db.ssl };
 var eezy_connection =  new pg.Client(dbConnectionConfig);
 eezy_connection.connect();
-
-
-exports.AuthenticateUser = function (username, password, callback) {
-    var qry = objcommon.SelectQuery('users', '*', "\"isAdmin\"=true and email ='"+username+ "' and password='" + cryptr.encrypt(password) + "'");
-    eezy_connection.query(qry, function (err, result) {
-        callback(result);
-    })
-}
-
-
-exports.GetAllUsersForAdmin = function (callback) {
-    var qry = objcommon.SelectQuery('users', '*','"isAdmin"=false','id desc');
-    eezy_connection.query(qry, function (err, result) {
-        callback(result);
-    })
-}
--- routes/admin/login.js
+++ routes/admin/login.js
@@ -3,7 +3,7 @@
 */
 const express = require('express');
 const router = express.Router();
-const users = require('../../Models/users');
+
 /**
  * Login page
 * @method Login
@@ -11,16 +11,5 @@ const users = require('../../Models/users');
 router.get('/login', function (req, res, next) {
   res.render('admin/login', { AdminTitle: ':: Admin : Login ::'});
 });
-
-// router.post('/login', function (req, res, next) {
-//   users.AuthenticateUser(req.body.username, req.body.password, function (result) {
-//     if (result.rowCount > 0) {
-//       res.redirect('/admin/dashboard');
-//     }
-//     else {
-//         res.render('admin/login', { AdminTitle: ':: Admin : Login ::', message: 'Incorrect username or password', style: 'error-msg'});
-//     }
-//   });
-// });
 
 module.exports = router;
